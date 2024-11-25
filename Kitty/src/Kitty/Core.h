e4d302df741bb37c9e1647210b0fed67a474072f
Krishna <krishfps9222@gmail.com>
2024-11-26 01:11:00 +0530

    updated Core.h

--- Kitty/src/Kitty/Core.h
+++ Kitty/src/Kitty/Core.h
@@ -9,3 +9,12 @@
 #else
 	#error Invalid Platform!
 #endif
+
+
+/*
+Explanation:
+	If engine ( KITTY_PLATFORM_WIN and KITTY_BUILD_DLL)
+		__declspec(dllexport)
+	Else (KITTY_PLATFORM_WIN) 
+		__declspec(dllimport)
+*/
