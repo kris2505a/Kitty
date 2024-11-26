109fdb4dfa1715d52b0d3cb068a86e2f7466fa18
Krishna <krishfps9222@gmail.com>
2024-11-27 00:32:59 +0530

    added premake

--- Kitty/src/Kitty/EntryPoint.h
+++ Kitty/src/Kitty/EntryPoint.h
@@ -8,7 +8,7 @@ extern Kitty::Application* Kitty::createApplication();
 int main(int argc, char** argv) {
 	Kitty::Log::init();
 	KITTY_CORE_ERROR("Kitty Error");
-	KITTY_FATAL("Kitty App LOG");
+	KITTY_TRACE("Kitty App LOG");
 	auto app = Kitty::createApplication();
 	app->run();
 	delete app;
---
+++ premake5.lua
@@ -0,0 +1,100 @@
+workspace "Kitty"
+    architecture "x64"
+
+    configurations{
+        "Debug",
+        "Release",
+        "Dist"
+    }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Kitty"
+    location "Kitty"
+    kind "SharedLib"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("binInt/" .. outputdir .. "/%{prj.name}")
+
+    files{
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs{
+        "%{prj.name}/vendor/spdlog/include"
+    }
+
+    filter "system:windows"
+        cppdialect "C++23"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines{
+            "KITTY_PLATFORM_WIN",
+            "KITTY_BUILD_DLL"
+        }
+
+        postbuildcommands{
+            ("{COPY} %{cfg.buildtarget.relpath} ../bin/" ..outputdir .. "/SandBox")    
+        }
+
+
+    filter "configurations:Debug"
+        defines "KITTY_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "KITTY_RELEASE"
+        optimize "on"
+    
+    filter "configurations:Dist"
+        defines "KITTY_DIST"
+        optimize "On"
+
+project "SandBox"
+        location "SandBox"
+        kind "ConsoleApp"
+        language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("binInt/" .. outputdir .. "/%{prj.name}")
+
+    files{
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs{
+        "Kitty/vendor/spdlog/include",
+        "Kitty/src"
+    }
+
+    links{
+        "Kitty"
+    }
+
+    filter "system:windows"
+        cppdialect "C++23"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines{
+            "KITTY_PLATFORM_WIN",
+            
+        }
+
+
+    filter "configurations:Debug"
+        defines "KITTY_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "KITTY_RELEASE"
+        optimize "on"
+    
+    filter "configurations:Dist"
+        defines "KITTY_DIST"
+        optimize "On"
+
